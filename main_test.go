2736efa87c4eecca3421ffa771e8d926730d4355
Sergey <dyuzhovsergey@gmail.com>
2025-07-31 19:08:45 +0300

    Create test func TestMaximum

--- main_test.go
+++ main_test.go
@@ -7,11 +7,54 @@ import (
 
 func TestGenerateRandomElements(t *testing.T) {
 	empty := generateRandomElements(0)
-	if empty != nil {
-		t.Errorf("For size 0, should be returned nil")
+	if len(empty) != 0 {
+		t.Errorf("For size 0, should be returned empty slise")
 	}
 	size10 := generateRandomElements(10)
 	if len(size10) != 10 {
-		t.Errorf("Expected length of 88, received: %d", len(size10))
+		t.Errorf("Expected length of 10, received: %d", len(size10))
+	}
+}
+
+func TestMaximum(t *testing.T) {
+	if max := maximum([]int{}); max != 0 {
+		t.Errorf("For empty slice, was expected 0, received %d", max)
+	}
+
+	if max := maximum([]int{4}); max != 4 {
+		t.Errorf("Expected 4, received %d", max)
+	}
+
+	if max := maximum([]int{1, 8, 2, 6, 4}); max != 8 {
+		t.Errorf("Expected 8, received %d", max)
+	}
+
+	if max := maximum([]int{7, 7, 7, 7}); max != 7 {
+		t.Errorf("Expected 7, received %d", max)
+	}
+}
+func TestMaxChunks(t *testing.T) {
+	if max := maxChunks([]int{}); max != 0 {
+		t.Errorf("For empty slice, was expected 0, received %d", max)
+	}
+
+	if max := maxChunks([]int{1, 2, 3}); max != 3 {
+		t.Errorf("Expected 3, received %d", max)
+	}
+
+	testData := make([]int, CHUNKS)
+	for i := range testData {
+		testData[i] = i + 1
+	}
+	if max := maxChunks(testData); max != CHUNKS {
+		t.Errorf("Expected %d, received %d", CHUNKS, max)
+	}
+
+	largeData := make([]int, 100)
+	for i := range largeData {
+		largeData[i] = i + 1
+	}
+	if max := maxChunks(largeData); max != 100 {
+		t.Errorf("Expected 100, received %d", max)
 	}
 }
